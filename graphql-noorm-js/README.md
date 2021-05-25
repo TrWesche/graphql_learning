0fb7ef9e2bc3bfd532beea735cbb737b1faff270
Trenton Wesche <trwes@U0134-M21P-01.a-cc.com>
2021-05-24 20:04:35 -0500

    db stuff

--- graphql-noorm-js/README.md
+++ graphql-noorm-js/README.md
@@ -1,7 +1,5 @@
 Schema Modularization w/ Typescript:
-https://pusher.com/tutorials/graphql-typescript
-https://typegraphql.com/
-
+https://mikewilliamson.wordpress.com/2017/03/24/arangodb-and-graphql/
 
 https://www.apollographql.com/blog/modularizing-your-graphql-schema-code-d7f71d5ed5f2/
 
---
+++ graphql-noorm-js/src/repositories/comment.repo.js
@@ -0,0 +1,20 @@
+import db from '../db';
+import { aql } from 'arangojs'
+
+class CommentRepo {
+    static async getPersonByName(name) {
+        let query = aql`
+            FOR person IN persons
+                FILTER person.name == ${ name }
+                LIMIT 1
+                RETURN person
+            `;
+        let results = await db.query(query);
+        return results.next();
+    }
+
+    
+
+}
+
+export default CommentRepo;
