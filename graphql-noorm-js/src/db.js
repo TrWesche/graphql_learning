a7152c305e4b2e96525d9b62b1306a86506c6f3e
Trenton Wesche <trwes@U0134-M21P-01.a-cc.com>
2021-06-28 22:26:16 -0500

    Docker compose working

--- graphql-noorm-js/src/db.js
+++ graphql-noorm-js/src/db.js
@@ -1,26 +1,11 @@
 import { Database } from 'arangojs';
 import { ARANGODB_ADDRESS, ARANGODB_NAME, ARANGODB_USER, ARANGODB_PASSWORD } from './config';
 
-console.log(`
-  Connecting to Database:
-  Address: ${ARANGODB_ADDRESS}
-  Database Name: ${ARANGODB_NAME}
-  Database User: ${ARANGODB_USER}
-  Database Password: ${ARANGODB_PASSWORD}
-`)
-
 const db = new Database({
   url: ARANGODB_ADDRESS,
   databaseName: ARANGODB_NAME,
   auth: {username: ARANGODB_USER, password: ARANGODB_PASSWORD}
 })
-
-// console.log(`
-//   Database Connected:
-//   Version: ${db.version()}
-//   Attempting Login:
-//   ${db.login(ARANGODB_USER, ARANGODB_PASSWORD)}
-// `)
 
 // Define Database Collections
 // Document Collections
